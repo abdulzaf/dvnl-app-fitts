c49c7f9a3dc615d0794e1d0c3d35601fc9aeef00
Abdullah Zafar <zafar.abdullah10@gmail.com>
2023-03-06 17:00:13 -0500

    updates

--- Python/event_detection.py
+++ Python/event_detection.py
@@ -1,68 +1,60 @@
 # -*- coding: utf-8 -*-
 """
-Created on Thu Mar  2 15:06:08 2023
+Created on Mon Mar  6 16:39:23 2023
 
 @author: zafar
 """
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-import os
-from scipy.ndimage import label
 from scipy.signal import find_peaks
+from scipy.ndimage import label
+
+# HELPER FUNCTIONS
+def getOnsetOffset(x: np.array, threshold: float) -> (list, list):
+    x = np.abs(x.copy())
+    x[x<threshold] = 0
+    
+    lbl, num_lbl = label(x)
+    onsets = []
+    offsets = []
+    for i in range(num_lbl):
+        idx = np.where(lbl==(i+1))[0]
+        event_time = idx[-1] - idx[0]
+        if event_time > 50:
+            onsets.append(idx[0])
+            offsets.append(idx[-1])
+    
+    return onsets, offsets
 
 # Read
-df_vel = pd.read_csv('./pilot.csv')
-df_pos = pd.read_csv('./pilot_pos.csv')
+trial_no = 1
+df = pd.read_csv(f'./fitts_ot_{trial_no}.csv')
+df = df.interpolate(method='cubic')
 
-x = df_pos['finger x']
-v = df_vel['finger x']
+FS = 250 # Hertz
+X = df.x.values # position: mm
+T = df.time.values # time: sec
+V = np.gradient(X) * FS # velocity: mm/s
 
-# Outcome measures
+# RELEVANT WINDOW
+TIME_START = 1 # seconds
+TIME_END = 8 # seconds
+X = X[np.logical_and(T>TIME_START, T<TIME_END)]
+V = V[np.logical_and(T>TIME_START, T<TIME_END)]
+T = T[np.logical_and(T>TIME_START, T<TIME_END)]
+
+#% Find thresholds
 # Movement onset: >20mm/s
-v_on_20 = np.abs(v.copy())
-v_on_20[v_on_20<20] = 0
-v_on_20[:500] = 0
-v_on_20[1500:] = 0
-
-lbl, num_lbl = label(v_on_20.values)
-move_on_20 = []
-move_off_20 = []
-for i in range(num_lbl):
-    idx = np.where(lbl==(i+1))[0]
-    move_time = idx[-1] - idx[0]
-    if move_time > 10:
-        move_on_20.append(idx[0])
-        move_off_20.append(idx[-1])
-
+on_20, off_20 = getOnsetOffset(V, 20)
 # Movement onset: >100mm/s
-v_on_100 = np.abs(v.copy())
-v_on_100[v_on_100<100] = 0
-v_on_100[:500] = 0
-v_on_100[1500:] = 0
-
-lbl, num_lbl = label(v_on_100.values)
-move_off_100 = []
-for i in range(num_lbl):
-    idx = np.where(lbl==(i+1))[0]
-    move_time = idx[-1] - idx[0]
-    if move_time > 10:
-        move_off_100.append(idx[-1])
+on_100, off_100 = getOnsetOffset(V, 100)
+# Peak velocities
+pk_vel, _ = find_peaks(np.abs(V), height=500, distance=20)
         
-# Peak velocity
-pk_vel, _ = find_peaks(np.abs(v), height=500, distance=20)
-
-# plt.plot(v_on)
-# plt.hlines(20, 0, len(v_on))
-# Movement offset: <20mm/s
-x -= x[0]
-plt.figure()
-plt.plot(x)
-plt.plot(move_on_20, x[move_on_20], 'go')
-plt.plot(move_off_20, x[move_off_20], 'ro')
-plt.plot(move_off_100, x[move_off_100], 'mo')
-plt.plot(pk_vel, x[pk_vel], 'ko')
-# plt.vlines(move_on_20, -75, 75, color='g')
-# plt.vlines(move_off_20, -75, 75, color='r')
-# plt.vlines(move_off_100, -75, 75, color='m')
-# plt.plot(label)
+plt.plot(T, X)
+plt.vlines(T[on_20], -125, 125, color='g')
+plt.plot(T[on_20], X[on_20], 'go')
+plt.plot(T[off_20], X[off_20], 'ro')
+plt.plot(T[off_100], X[off_100], 'mo')
+plt.plot(T[pk_vel], X[pk_vel], 'ko')
